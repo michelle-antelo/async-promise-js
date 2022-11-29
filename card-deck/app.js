1adb068cde5cdd03d93f9fbda845569f15cc166e
Michelle Antelo <michelle@Michelles-MacBook-Pro.local>
2022-11-29 15:42:22 -0500

    promise/async

--- card-deck/app.js
+++ card-deck/app.js
@@ -1,11 +1,20 @@
 $(function() {
     let baseURL = 'https://deckofcardsapi.com/api/deck';
   
+    // 1
     $.getJSON(`${baseURL}/new/draw/`).then(data => {
       let { suit, value } = data.cards[0];
       console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
     });
+
+    // async
+    async function part1() {
+        let data = await $.getJSON(`${baseURL}/new/draw/`);
+        let { suit, value } = data.cards[0];
+        console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+      }
   
+    // 1
     let firstCard = null;
     $.getJSON(`${baseURL}/new/draw/`)
       .then(data => {
@@ -21,7 +30,19 @@ $(function() {
           );
         });
       });
-  
+      
+    // async
+    async function part2() {
+    let firstCardData = await $.getJSON(`${baseURL}/new/draw/`);
+    let deckId = firstCardData.deck_id;
+    let secondCardData = await $.getJSON(`${baseURL}/${deckId}/draw/`);
+    [firstCardData, secondCardData].forEach(card => {
+        let { suit, value } = card.cards[0];
+        console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+    });
+    }
+
+    // 1
     let deckId = null;
     let $btn = $('button');
     let $cardArea = $('#card-area');
@@ -48,4 +69,29 @@ $(function() {
         if (data.remaining === 0) $btn.remove();
       });
     });
-  });
+
+  // async
+  async function setup() {
+    let $btn = $('button');
+    let $cardArea = $('#card-area');
+
+    let deckData = await $.getJSON(`${baseURL}/new/shuffle/`);
+    $btn.show().on('click', async function() {
+      let cardData = await $.getJSON(`${baseURL}/${deckData.deck_id}/draw/`);
+      let cardSrc = cardData.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      $cardArea.append(
+        $('<img>', {
+          src: cardSrc,
+          css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+          }
+        })
+      );
+      if (cardData.remaining === 0) $btn.remove();
+    });
+  }
+  setup();
+});
--- number-facts/app.js
+++ number-facts/app.js
@@ -1,15 +1,32 @@
 let favNumber = 5;
 let baseURL = "http://numbersapi.com";
 
+// 1
 $.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
   console.log(data);
 });
 
+// async
+async function part1() {
+  let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
+  console.log(data);
+}
+part1();
+
+// 1
 let favNumbers = [7, 11, 22];
 $.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
   console.log(data);
 });
 
+// async
+async function part2() {
+  let data = await $.getJSON(`${baseURL}/${favNumbers}?json`);
+  console.log(data);
+}
+part2();
+
+// 1
 Promise.all(
   Array.from({ length: 4 }, () => {
     return $.getJSON(`${baseURL}/${favNumber}?json`);
@@ -17,3 +34,14 @@ Promise.all(
 ).then(facts => {
   facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
 });
+
+// async
+async function part3() {
+  let facts = await Promise.all(
+    Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
+  );
+  facts.forEach(data => {
+    $('body').append(`<p>${data.text}</p>`);
+  });
+}
+part3();
